ec988aaaaf9d930c0282bc800e21a1541efd30a3
abelBEDOYA <abelgonzalezbernad@gmail.com>
2023-05-10 18:57:11 +0200

    train.py funcional desde terminal y argsparse

--- app/train.py
+++ app/train.py
@@ -6,6 +6,7 @@ import numpy as np
 import os
 from tqdm import tqdm
 import random
+import argparse
 
 # Mi libreria:
 from processLIDC import Patient
@@ -38,7 +39,7 @@ def get_val_loss(model, val_patients, loss_fn, batch_size=4):
     batch_loss_history = np.array([])
     loss_patient = np.array([])
 
-    for id_pat in tqdm(val_patients):
+    for id_pat in val_patients:
             # Cargamos datos de un paciente:
             patient = Patient(id_pat)
             
@@ -50,7 +51,7 @@ def get_val_loss(model, val_patients, loss_fn, batch_size=4):
             
             # Preparamos tensores para recorrerlos:
             primera = 2
-            ultima = 6
+            ultima = 10
             dataset = TensorDataset(imgs[primera:ultima], mask[primera:ultima])
             # dataset = TensorDataset(imgs, mask)
             
@@ -70,10 +71,41 @@ def get_val_loss(model, val_patients, loss_fn, batch_size=4):
     val_mean_loss = np.mean(loss_patient)
     return val_mean_loss
 
+def plot(data, show=False, path_save=None):
+    epoch_loss_history = data['epoch_loss_history']
+    batch_loss_history = data['batch_loss_history']
+    patient_loss_history = data['patient_loss_history']
+    epoch_val_loss_history = data['epoch_val_loss_history']
+    n_epochs = len(epoch_loss_history)
+    plt.plot(np.linspace(1, n_epochs, np.array(batch_loss_history).shape[0]), np.log(np.array(batch_loss_history)), label='Train Batch Loss')
+    plt.plot(np.linspace(1, n_epochs, np.array(patient_loss_history).shape[0]), np.log(np.array(patient_loss_history)), label='Train Patient Loss')
+    plt.plot(np.linspace(1, n_epochs, n_epochs), np.log(np.array(epoch_loss_history)), label='Train Epoch Loss')
+    plt.plot(np.linspace(1, n_epochs, n_epochs), np.log(np.array(epoch_val_loss_history)), label='Val. Epoch Loss')
+    plt.title('Loss: Binary Cross Entropy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(loc = 'best', frameon=True)
+    if path_save is not None:
+        plt.savefig(path_save+'loss_plot.png', dpi=300)
+    if show:
+        plt.show()
+
+
+
+def save_model(model, path='./', model_name='model'):
+
+    # Guardar el modelo
+    torch.save(model.state_dict(), path+model_name+'.pth')
+    print('Modelo: {}{}.pth guardado.'.format(path, model_name))
+
 def train(model, n_epochs:int =4, 
-          batch_size: int =4, 
+          batch_size: int = 4, 
           val_split: float = 0.2,
-          path2dataset: str = '../../manifest-1675801116903/LIDC-IDRI/'):
+          path2dataset: str = '../../manifest-1675801116903/LIDC-IDRI/',
+          path2savefiles: str = './',
+          plot_metrics: bool = False,
+          save_plots: bool = False,
+          save_epochs = None):
     """Ejecuta el entrenamiento
 
     Args:
@@ -81,8 +113,11 @@ def train(model, n_epochs:int =4,
         epochs (int, optional): numero de epocas. Defaults to 4.
         batch_size (int, optional): batch de imagenes (no pacientes) a evaluar antes de haer backprop. Defaults to 4.
         val_split (float, optional): porcentaje del dataset a validation. Defaults to 0.2.
+        path2dataset: str = '../../manifest-1675801116903/LIDC-IDRI/',
+        plot: bool = False,
+        save_plots: bool = False)
     """
-    patients = os.listdir('../../manifest-1675801116903/LIDC-IDRI/')
+    patients = os.listdir(path2dataset)
     patients = [pat for pat in patients if not pat=='LICENSE']
 
     train_patients, val_patients = train_val_split(patients, val_split)
@@ -119,7 +154,7 @@ def train(model, n_epochs:int =4,
             
             # Preparamos tensores para recorrerlos:
             primera = 2
-            ultima = 6
+            ultima = 10
             dataset = TensorDataset(imgs[primera:ultima], mask[primera:ultima])
             # dataset = TensorDataset(imgs, mask)
             
@@ -145,11 +180,54 @@ def train(model, n_epochs:int =4,
             patient_loss_history = np.append(patient_loss_history, np.mean(np.array(loss_batch)))
             
         epoch_loss_history = np.append(epoch_loss_history, np.mean(np.array(loss_patient)))
-        
-        # Calculemos el loss del val:
+        if save_epochs is not None:
+            if epoch//save_epochs == epoch/save_epochs:
+                save_model(model, path2savefiles, model_name= 'model-epoch{}'.format(epoch))
+        # # Calculemos el loss del val:
         val_loss = get_val_loss(model, val_patients, loss_fn, batch_size)
         epoch_val_loss_history = np.append(epoch_val_loss_history, val_loss)
-        
         print('Train Epoch: {}\t Train Loss: {:.6f}. Val Loss: {:.6f}'.format(
-            epoch, epoch_loss_history[-1], epoch_val_loss_history))
+            epoch, epoch_loss_history[-1], epoch_val_loss_history[-1]))
         print('-----------------------------------')
+    data_dict ={
+                'epoch_loss_history': epoch_loss_history,
+                'batch_loss_history': batch_loss_history,
+                'patient_loss_history': patient_loss_history,
+                'epoch_val_loss_history': epoch_val_loss_history
+                }
+    save_model(model, path2savefiles, model_name='finalmodel')
+    if save_plots:
+        plot(data_dict, show=plot_metrics, path_save=path2savefiles)
+    else:
+        plot(data_dict, show=plot_metrics)
+            
+        
+if __name__=='main__':
+    parser = argparse.ArgumentParser()
+
+    # Agregar los argumentos necesarios
+    parser.add_argument('--n_epochs', type=int, default=4)
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--val_split', type=float, default=0.2)
+    parser.add_argument('--path2dataset', type=str, default='../../manifest-1675801116903/LIDC-IDRI/')
+    parser.add_argument('--path2savefiles', type=str, default='./')
+    parser.add_argument('--plot_metrics', action='store_true')
+    parser.add_argument('--save_plots', action='store_true')
+    parser.add_argument('--save_epochs', type=int, default=None)
+
+    # Obtener los argumentos proporcionados por el usuario
+    args = parser.parse_args()
+    
+    # Descargamos el modelo preentrenado:
+    model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
+                       in_channels=3, out_channels=1, init_features=32, pretrained=True)
+    
+    # Llamar a la función train con los argumentos
+    train(model, n_epochs=args.n_epochs, batch_size=args.batch_size, val_split=args.val_split,
+        path2dataset=args.path2dataset, path2savefiles=args.path2savefiles,
+        plot_metrics=args.plot_metrics, save_plots=args.save_plots,
+        save_epochs=args.save_epochs)
+        
+        
+        
+
